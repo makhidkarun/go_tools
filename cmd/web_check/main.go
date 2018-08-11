873083dcf1094d89751d1ce9cc8096fbef4e6248
Leam Hall <leamhall@gmail.com>
2018-08-10 22:15:26 -0400

    Item 4 in web_check.

--- cmd/web_check/main.go
+++ cmd/web_check/main.go
@@ -1,4 +1,4 @@
-// web_check.go
+// web_check
 // Used to test URLs to ensure they have relevant code.
 // Starting from the example on page 93 of Caleb Doxsey's 
 // "Introducing Go".
@@ -6,14 +6,20 @@
 // TODO:
 // 1.  Make it work. DONE
 // 2.  Grab response codes. DONE
-// 3.  Check for key words in response.Body.
-// 4.  Take a file of URLs and key words
-// 5.  Set up base round of tests.
+// 3.  Check for key words in response.Body. DONE
+// 4.  Take a file of URLs. DONE 
+// 4.1  Error checking.
+// 4.2  Check for and add http:// if missing. Or work around.
+// 5.  Add key words to data file. (?csv)
+// 6.  Set up base round of tests.
 
 package main
 
 import (
+	"bufio"
+	"flag"
   "fmt"
+	"os"
   "io/ioutil"
   "net/http"
   "strings"
@@ -26,18 +32,33 @@ type HomePageSize struct {
 	Body			[]byte
 }
 
+var file = flag.String("f", "-", "File to read")
+
 func has_string(body []byte, term string) bool {
 	body_string := string(body[:len(body)])
 	return strings.Contains(body_string, term)
 }
 
+func lines(file *os.File) (lines []string)  {
+	input := bufio.NewScanner(file)
+	for input.Scan() {
+		my_string := input.Text()
+		if len(my_string) > 2 {
+			lines = append(lines, my_string)
+		}
+	}
+	return
+}
 func main() {
-  urls := []string{
-    "http://www.apple.com",
-    "http://www.amazon.com",
-    "http://www.google.com",
-    "http://microsoft.com",
-  }
+	flag.Parse()
+	data, err := os.Open(*file)
+	if err != nil {
+		fmt.Fprint(os.Stderr, "Open failed: %v\n", err)
+		os.Exit(1)
+	}
+	defer data.Close()
+
+	urls := lines(data)
 
   results := make(chan HomePageSize)
 
@@ -54,7 +75,7 @@ func main() {
         panic(err)
       }
 			resp := res.StatusCode
-      
+
 			results <- HomePageSize{
         URL:  url,
         Size: len(bs),
@@ -70,4 +91,6 @@ func main() {
 		fmt.Println(has_string(result.Body, "doufdofudoifud"))  // Should give false
 		fmt.Println(has_string(result.Body, "google")) 				  // Should usually give true
   }
+	
+
 }
