b44474bc4fdc6157626a1b64371725f5ca2aa0c0
Leam Hall <leamhall@gmail.com>
2019-01-30 07:42:22 -0500

    Ran go fmt on person and functions.

--- functions_test.go
+++ functions_test.go
@@ -1,65 +1,64 @@
 package go_tools
 
 import (
-  mrand "math/rand"
+	mrand "math/rand"
 
-  "github.com/makhidkarun/go_tools"
-  "testing"
+	"github.com/makhidkarun/go_tools"
+	"testing"
 )
 
+var rng = mrand.New(mrand.NewSource(99))
 
-var rng  = mrand.New(mrand.NewSource(99))
-
-func TestOneD6(t *testing.T){
-  roll := go_tools.OneD6()
-  if roll < 1 || roll > 6 {
-    t.Error(`OneD6 failed`)
-  }
+func TestOneD6(t *testing.T) {
+	roll := go_tools.OneD6()
+	if roll < 1 || roll > 6 {
+		t.Error(`OneD6 failed`)
+	}
 }
 
-func TestTwoD6(t *testing.T){
-  roll := go_tools.TwoD6()
-  if roll < 2 || roll > 12 {
-    t.Error(`TwoD6 failed`)
-  }
+func TestTwoD6(t *testing.T) {
+	roll := go_tools.TwoD6()
+	if roll < 2 || roll > 12 {
+		t.Error(`TwoD6 failed`)
+	}
 }
 
-func TestAge(t *testing.T){
-  age := go_tools.Age(2)
-  if age < 26 || age > 29 {
-    t.Error(`Age failed`)
-  }
+func TestAge(t *testing.T) {
+	age := go_tools.Age(2)
+	if age < 26 || age > 29 {
+		t.Error(`Age failed`)
+	}
 }
 
-func TestFormatUPP(t *testing.T){
-  upp := [6]int{ 7, 7, 15, 9, 12, 12}
-  newUPP := go_tools.FormatUPP(upp)
-  if newUPP != "77F9CC" {
-    t.Error(`FormatUPP failed.`)
-  }
+func TestFormatUPP(t *testing.T) {
+	upp := [6]int{7, 7, 15, 9, 12, 12}
+	newUPP := go_tools.FormatUPP(upp)
+	if newUPP != "77F9CC" {
+		t.Error(`FormatUPP failed.`)
+	}
 }
 
-func TestStringInArray(t *testing.T){
-  var genders []string = []string{"F", "M"}
-  option1 := "F"
-  option2 := "R"
-  if !go_tools.StringInArray(option1, genders){
-    t.Error(`Missing an F.`)
-  }
-  if go_tools.StringInArray(option2, genders){
-    t.Error(`Never met R.`)
-  }
+func TestStringInArray(t *testing.T) {
+	var genders []string = []string{"F", "M"}
+	option1 := "F"
+	option2 := "R"
+	if !go_tools.StringInArray(option1, genders) {
+		t.Error(`Missing an F.`)
+	}
+	if go_tools.StringInArray(option2, genders) {
+		t.Error(`Never met R.`)
+	}
 }
 
-func TestRandomStringFromArray(t *testing.T){
-  var genders []string = []string{"F", "M"}
-  gender := go_tools.RandomStringFromArray(genders)
-  if !go_tools.StringInArray(gender, genders){
-    t.Error(`Bad gender output.`)
-  }
-  var ranks []string = []string{"PVT", "SGT", "LT", "CPT", "MAJ"}
-  rank := go_tools.RandomStringFromArray(ranks)
-  if !go_tools.StringInArray(rank, ranks){
-    t.Error(`Bad rank output.`)
-  }
+func TestRandomStringFromArray(t *testing.T) {
+	var genders []string = []string{"F", "M"}
+	gender := go_tools.RandomStringFromArray(genders)
+	if !go_tools.StringInArray(gender, genders) {
+		t.Error(`Bad gender output.`)
+	}
+	var ranks []string = []string{"PVT", "SGT", "LT", "CPT", "MAJ"}
+	rank := go_tools.RandomStringFromArray(ranks)
+	if !go_tools.StringInArray(rank, ranks) {
+		t.Error(`Bad rank output.`)
+	}
 }
--- person.go
+++ person.go
@@ -1,6 +1,6 @@
 // To create a basic 2d6 OGL character
 
-// With acknowledgement of Marc Miller of FFE, 
+// With acknowledgement of Marc Miller of FFE,
 // Jason "Flynn" Kemp of Cepheus Engine,
 // Rob Pike and the Google Go team,
 // and Freenode#go-nuts.
@@ -8,45 +8,45 @@
 package go_tools
 
 import (
-  "strconv"
-  "strings"
+	"strconv"
+	"strings"
 )
 
 type Person struct {
-  Name    string
-  UPP     [6]int
-  Gender  string
-  PSR     int
-  Age     int
-  Terms   int
-  Career  string
+	Name   string
+	UPP    [6]int
+	Gender string
+	PSR    int
+	Age    int
+	Terms  int
+	Career string
 }
 
 func MakePerson(options map[string]string) Person {
-  terms, _  := strconv.Atoi(options["terms"])
-  gender    := options["gender"]
-  db_name   := options["db_name"]
+	terms, _ := strconv.Atoi(options["terms"])
+	gender := options["gender"]
+	db_name := options["db_name"]
 
-  var character Person 
+	var character Person
 
-  // Need to figure out pre-adult characters
-  if terms <= 0 {
-    character.Terms = NumTerms()
-  } else {
-    character.Terms = terms
-  }
+	// Need to figure out pre-adult characters
+	if terms <= 0 {
+		character.Terms = NumTerms()
+	} else {
+		character.Terms = terms
+	}
 
-  var genders []string = []string{"F", "M"}
-  input_gender := strings.ToUpper(gender)
-  if !StringInArray(input_gender, genders) {
-    character.Gender = Gender()
-  } else {
-    character.Gender = input_gender
-  }
+	var genders []string = []string{"F", "M"}
+	input_gender := strings.ToUpper(gender)
+	if !StringInArray(input_gender, genders) {
+		character.Gender = Gender()
+	} else {
+		character.Gender = input_gender
+	}
 
-  character.Name    = GetName(character.Gender, db_name)
-  character.UPP     = RollUPP()
-  character.Age     = Age(character.Terms)
+	character.Name = GetName(character.Gender, db_name)
+	character.UPP = RollUPP()
+	character.Age = Age(character.Terms)
 
-  return character
+	return character
 }
